1689632252b5b30ce8faa75e65ac8f9a0b0a16ca
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-09 21:28:58 +0600

    learned about DOM traversing

--- module-24-DOM-Intro/js/DOM.js
+++ module-24-DOM-Intro/js/DOM.js
@@ -1,1 +1,28 @@
-console.log(`Hello from JS`);
+/**********************
+ * getElementsByTagName
+ **********************/
+
+// ul, li, h1, p, etc. aigula shob kintu tags.
+
+/*
+const all_ul = document.getElementsByTagName("li");
+
+console.log(all_ul);
+*/
+
+/**********************
+ * getElementById
+ **********************/
+
+const programTitle = document.getElementById("programs-title");
+
+programTitle.style.color = "red";
+programTitle.style.backgroundColor = "yellow";
+
+console.log(programTitle.getAttribute("id"));
+
+/**********************
+ * getElementsByClassName
+ **********************/
+
+const fruitItem = document.getElementsByClassName("fruit-item");
