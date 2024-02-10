6585a54562dfce81b21f94461e9eca83da15d95d
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-10 13:48:39 +0600

    node list learning

--- module-24-DOM-Intro/js/styleInJs.js
+++ module-24-DOM-Intro/js/styleInJs.js
@@ -1,8 +1,33 @@
 const sections = document.querySelectorAll("section");
-console.log(sections);
 
 for (const iterator of sections) {
    iterator.style.border = "1px solid black";
    iterator.style.marginBottom = "20px";
    iterator.style.padding = "20px";
 }
+
+/**
+ ********* Node List
+
+ * Every HTML element is a Node in DOM.
+ * Some important and regularly used properties of Nodes are:
+ * 1. Node.firstChild
+ * 2. Node.childNodes
+ * 3. 
+
+
+ */
+
+const languageContainer = document.getElementById("language-container");
+
+// Grab the UL to which we want to add the list item
+const myUL = document.querySelector("#language-container ul");
+
+// Create a List Element through JS
+const myLi = document.createElement("li");
+
+// Give some text inside the created List Element
+myLi.innerText = "List Created By JavaScript";
+
+// Add the created List Element to the UL
+myUL.appendChild(myLi);
